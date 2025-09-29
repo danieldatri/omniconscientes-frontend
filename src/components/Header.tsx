6c2aee31659cc856865ce53b2c49b87b09b3f39d
Daniel Alejandro Datri <daniel.datri@gmail.com>
2025-09-29 19:41:33 -0300

    feat: enhance header with logo integration and responsive styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Image from 'next/image';
 import styles from './Header.module.css';
 import ThemeSwitcher from './ThemeSwitcher';
 
 const Header: React.FC = () => (
   <header className={styles.header}>
-    <div className={styles.left}>omniconscientes</div>
+    <div className={styles.left}>
+      <Image src="/logo.svg" alt="Logo Omniconscientes" width={64} height={64} className={styles.logo} priority />
+      <span className={styles.brand}>omniconscientes</span>
+    </div>
     <div className={styles.right}>
       <button className={styles.button}>Acceder</button>
       <button className={styles.button}>Registrarse</button>
